4b3e24286d02084364e962c16b0cfcacdd699245
MarinaPaley <marina_paley@mail.ru>
2022-11-23 20:27:12 +0300

    Сделана лр #9

---
+++ Works/Task 4/SQLQuery3.sql
@@ -0,0 +1,9 @@
+﻿--Показать количество городов и штат (StateProvinceID), в котором находится максимальное количество городов (Таблица Person.[Address])
+SELECT TOP 1
+	s.[Name]
+  , COUNT(City) AS Cities
+FROM Person.[Address] AS a
+INNER JOIN Person.StateProvince AS s
+ON s.StateProvinceID = a.StateProvinceID
+GROUP BY s.[Name]
+ORDER BY 2 DESC;
